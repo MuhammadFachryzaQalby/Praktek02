b8d369166e02affe3a1c6acecb62aed93ccf3e50
qalbyreza@gmail.com <qalbyreza@gmail.com>
2023-05-26 10:10:38 +0800

    constuctor - sama Dengan Nama Class

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -4,6 +4,11 @@ public class Rectangle {
     double panjang;
     double lebar;
 
+    public Rectangle() {
+        panjang=5;
+        lebar=2;
+    }
+
      void cetakInfo(){
          System.out.println("=========================");
          System.out.println("Panjang : " + panjang);
